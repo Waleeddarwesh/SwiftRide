4412515f859f3809aa5af48709f4eaeb16a43557
Waleeddarwesh <138933390+Waleeddarwesh@users.noreply.github.com>
2024-08-13 22:58:12 +0300

    Delete tickets directory

--- tickets/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns=[
-    path('search-tickets/', TicketSearchView.as_view(), name='search-tickets'),
-    path('trips/<int:trip_id>/seats/', TripSeatsView.as_view(), name='trip-seats'),
-    path('book-ticket/', TicketBookingView.as_view(), name='book-ticket'),
-]
