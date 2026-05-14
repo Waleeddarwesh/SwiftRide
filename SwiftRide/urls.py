34dbd3467f685e7846c8132af222611288d7a512
Waleed Darwesh <Waleeddarwesh2002@gmail.com>
2026-05-14 15:49:39 +0300

    edit swagger url to be standard

--- SwiftRide/urls.py
+++ SwiftRide/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('account/',include('account.urls')),
     path('tickets/',include('tickets.urls')),
     path('payment/',include('payment.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
